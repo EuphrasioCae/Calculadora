bcc7fb57f1bfca290af5ca197512ded05648a87d
Caê <134336937+EuphrasioCae@users.noreply.github.com>
2023-06-28 09:46:59 -0300

    Calculadora.js

--- Calculadora/Calculadora.js
+++ Calculadora/Calculadora.js
@@ -76,3 +76,59 @@ document.addEventListener('DOMContentLoaded', function () { //Importante colocar
     calculadora.inicia();
 });
 ///////////////////////////////////////////////////////////////////////
+
+/*Constructor function
+document.addEventListener('DOMContentLoaded', function () { --Importante colocar esta linha para o bom funcionamento do código
+    function Calculadora(){
+        this.display = document.querySelector('.display');
+
+        this.inicia = () => {
+            this.capturaCliques();
+            this.capturaEnter();
+        };
+
+        this.capturaCliques = () => {
+            document.addEvenListener('click', event => {
+                const el = event.target;
+                if(el.classList.contains('btn-num')) this.addNumDisplay(el);
+                if(el.classList.contains('btn-clear')) this.clear();
+                if(el.classList.contains('btn-del')) this.del();
+                if(el.classList.contains('btn-eq')) this.realizaConta();
+            });
+        };
+
+        this.realizaConta = () => {
+            try{
+                const conta = eval(this.display.value);
+                if(!conta){
+                    alert('Conta inválida');
+                }
+
+                this.display.value = conta;
+            } catch(e){
+                alert('Conta inválida);
+                return;
+            }
+        };
+
+        this.capturaEnter = () => {
+            this.display.addEventListener('keyup', e => {
+                if(e.keyCode !== 13) return;
+                this.realizaConta();
+            });
+        }
+
+        this.clear = () => this.display.value = '';
+
+        this.addNumDisplay = (el) => {
+             this.display.value += el.innerText;
+             this.display.focus();
+        };
+
+        this.del = () => this.display.value = this.display.value.slice(0, -1);
+    }
+
+    const calculadora = new Calculadora();
+    calculadora.inicia();
+});
+*/
